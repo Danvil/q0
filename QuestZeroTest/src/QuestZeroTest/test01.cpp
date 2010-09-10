4c1e21998c59f05a42510aae25297d26406a8970
weikersd <weikersd@a43ad8a5-d9bf-4b1e-9bae-79e1df8a1f23>
2010-09-10 18:44:26 +0000

    Added temporary test.
    git-svn-id: svn+ssh://svnradig.informatik.tu-muenchen.de/weikersd/QuestZero/trunk@2697 a43ad8a5-d9bf-4b1e-9bae-79e1df8a1f23

---
+++ QuestZeroTest/src/QuestZeroTest/test01.cpp
@@ -0,0 +1,46 @@
+/*
+ * test01.cpp
+ *
+ *  Created on: Sep 10, 2010
+ *      Author: david
+ */
+
+#include <QuestZero/Optimization.h>
+#include <QuestZero/IFunction.h>
+#include <QuestZero/Algorithms/RND.h>
+#include <QuestZero/Space/Cartesian.h>
+#include <Danvil/LinAlg.h>
+#include <boost/bind.hpp>
+
+typedef Danvil::ctLinAlg::Vec<double,17> state17d;
+typedef Danvil::ctLinAlg::Vec3f space3f;
+
+typedef Spaces::Cartesian::CartesianSpace<state17d> space17d;
+
+typedef FunctionFromDelegate<state17d> fnc17d;
+
+double sphere(const state17d& s)
+{
+	double sum = 0;
+	for(unsigned int i=0; i<s.dimension(); i++) {
+		double x = s[i];
+		sum += x * x;
+	}
+	return sum;
+}
+
+typedef Optimization<state17d, RND<state17d> > rndsolver;
+
+void foo()
+{
+	space17d myspace;
+	state17d myrange;
+	myrange.setAll(1);
+	myspace.setDomainRange(myrange);
+
+	PTR(fnc17d) myfnc(new fnc17d(boost::bind(&sphere, _1)));
+
+	rndsolver x;
+
+	state17d result = x.optimize<space17d,fnc17d>(myspace, myfnc).state();
+}
