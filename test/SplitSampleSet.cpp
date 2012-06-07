1523bcf03b96e1eede55cbf1dc8b1fab2f629d29
David Weikersdorfer <davidw@danvil.de>
2012-06-07 23:52:59 +0200

    Added test

---
+++ test/SplitSampleSet.cpp
@@ -0,0 +1,15 @@
+/*
+ * SplitSampleSet.cpp
+ *
+ *  Created on: Jun 7, 2012
+ *      Author: david
+ */
+
+#include <QuestZero/Common/DefaultSampleList.h>
+
+int main()
+{
+	Q0::TSampleSet<int, float> list;
+
+}
+
