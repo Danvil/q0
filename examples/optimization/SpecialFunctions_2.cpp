fd76b02ebd14a6ba721e50ed989f108aec97944d
David Weikersdorfer <davidw@danvil.de>
2012-06-15 14:32:39 +0200

    Added algorithm command line option to SpecialFunctions_2 example

--- examples/optimization/SpecialFunctions_2.cpp
+++ examples/optimization/SpecialFunctions_2.cpp
@@ -18,6 +18,7 @@ int main(int argc, char* argv[])
 {
 	unsigned int p_num_particles = 16384;
 	bool p_print_result_state = false;
+	std::string p_algo_name = "";
 	{
 		namespace po = boost::program_options;
 		po::options_description desc("Allowed options");
@@ -25,6 +26,7 @@ int main(int argc, char* argv[])
 			("help", "produce help message")
 			("num_particles", po::value<unsigned int>(&p_num_particles), "number of particles to use")
 			("verbose", po::value<bool>(&p_print_result_state), "print result vectors")
+			("algorithm", po::value<std::string>(&p_algo_name), "name of algorithm to use, empty string to run all")
 		;
 
 		po::variables_map vm;
@@ -40,62 +42,62 @@ int main(int argc, char* argv[])
 	std::cout << std::endl;
 	std::cout << "----- Null --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Null, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Null, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Sphere --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Sphere, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Sphere, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- DiscreetSphere --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::DiscreetSphere, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::DiscreetSphere, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Schwefel2_21 --- Expected result: (-inf,-inf) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel2_21, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel2_21, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Schwefel2_22 --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel2_22, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel2_22, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Schwefel1_2 --- Expected result: (inf,inf) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel1_2, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Schwefel1_2, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Rosenbrock --- Expected result: (1,1) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Rosenbrock, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Rosenbrock, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Himmelblau --- Expected result: (3,2), (-2.805118,3.131312), (-3.779310,-3.283186), (3.584428,-1.848126) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Himmelblau, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Himmelblau, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Rastrigin --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Rastrigin, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Rastrigin, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Step --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Step, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Step, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Ackley --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Ackley, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Ackley, p_num_particles, p_print_result_state, p_algo_name);
 
 	std::cout << std::endl;
 	std::cout << "----- Griewank --- Expected result: (0,0) -----" << std::endl;
 	std::cout << std::endl;
-	Solve(space, &Q0::SpecialFunctions<state_t>::Griewank, p_num_particles, p_print_result_state);
+	Solve(space, &Q0::SpecialFunctions<state_t>::Griewank, p_num_particles, p_print_result_state, p_algo_name);
 
 	return 1;
 }
