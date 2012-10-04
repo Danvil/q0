9ce658ca9366b32943069b911dfea0a056643a9f
David Weikersdorfer <davidw@danvil.de>
2012-10-04 13:31:13 +0200

    fixes

--- q0/algorithms.hpp
+++ q0/algorithms.hpp
@@ -1,6 +1,9 @@
 #ifndef Q0_ALGORITHMS_HPP_
 #define Q0_ALGORITHMS_HPP_
-#include <q0/algorithms/random_search.hpp>
+#include <q0/domains.hpp>
+#include <q0/objective.hpp>
+#include <q0/tools.hpp>
+#include <functional>
 //---------------------------------------------------------------------------
 namespace q0 {
 
@@ -10,6 +13,8 @@ struct minimize
 	template<typename Domain, typename Objective, typename Control, typename Compare=std::less<typename objective::result_type<Objective>::type>>
 	static inline particle<typename domains::state_type<Domain>::type,typename objective::result_type<Objective>::type>
 	apply(const Domain& domain, Objective f, Control control, Compare cmp=Compare()) {
+		// FIXME check typename domains::state_type<Domain>::type check1;
+		// FIXME check typename objective::result_type<Objective>::type check2;
 		return Algorithm<Domain,Objective,Control,Compare>::apply(domain, f, control, cmp);
 	}
 };
