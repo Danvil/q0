950f86caede4ae83e1e5afbe43636c936ed845f9
David Weikersdorfer <davidw@danvil.de>
2012-10-06 17:28:51 +0200

    Added new tests for more complex domains

---
+++ examples/test_algorithms_domains.cpp
@@ -0,0 +1,65 @@
+#include <q0/q0.hpp>
+#include <q0/algorithms.hpp>
+#include <q0/domains/cartesian.hpp>
+#include <q0/domains/so2.hpp>
+#include <q0/domains/vector.hpp>
+#include <q0/domains/tuple.hpp>
+#include <q0/algorithms/random_search.hpp>
+#include <q0/algorithms/apso.hpp>
+#include <q0/algorithms/differential_evolution.hpp>
+#include <iostream>
+
+typedef q0::domains::tuple<
+	q0::domains::cartesian<float,3>,
+	q0::domains::vector<q0::domains::so2<float>>
+> domain_t;
+typedef typename q0::domains::state_type<domain_t>::type state_t;
+
+std::ostream& operator<<(std::ostream& os, const state_t& x) {
+	os << "cartesian=" << std::get<0>(x).transpose() << ", ";
+	os << "so2 = {";
+	for(auto a : std::get<1>(x)) {
+		os << a << ", ";
+	}
+	os << "}";
+	return os;
+}
+
+unsigned int f_eval_count = 0;
+
+float f(const state_t& x) {
+	f_eval_count ++;
+	float sum = std::get<0>(x).squaredNorm();
+	for(unsigned int i=0; i<std::get<1>(x).size(); i++) {
+		sum += std::sin(.5f*std::get<1>(x)[i]); // has a minimum at 0!
+	}
+	return sum;
+}
+
+bool stop_condition(const state_t& u, float s) {
+//	std::cout << "{" << u.x() << "," << u.y() << "} -> " << s << std::endl;
+	return f_eval_count >= 10000 || s < 0.005f;
+}
+
+template<template<class,class,class,class>class Algo>
+void run(const std::string& name)
+{
+	f_eval_count = 0;
+	std::cout << "" << std::endl;
+	std::cout << ">>> " << name << std::endl;
+	domain_t dom;
+	std::get<1>(dom).resize(5); // 5 times so2
+	auto p = q0::minimize<Algo>::apply(dom, &f, q0::stop_condition(&stop_condition));
+	std::cout << "{" << p.state << "} -> " << p.score << std::endl;
+	std::cout << "Number of evaluations: " << f_eval_count << std::endl;
+}
+
+#define RUN(X) run<X>(#X)
+
+int main(int argc, char** argv)
+{
+	RUN(q0::algorithms::random_search);
+	RUN(q0::algorithms::apso);
+	RUN(q0::algorithms::differential_evolution);
+	return 1;
+}
---
+++ examples/test_domains.cpp
@@ -0,0 +1,54 @@
+#include <q0/q0.hpp>
+#include <q0/domains/cartesian.hpp>
+#include <q0/domains/so2.hpp>
+#include <q0/domains/vector.hpp>
+#include <q0/domains/tuple.hpp>
+#include <iostream>
+
+template<typename Domain>
+void run(const Domain& dom, const std::string& name)
+{
+	std::cout << std::endl;
+	std::cout << ">>> " << name << std::endl;
+	typedef typename q0::domains::state_type<Domain>::type state_t;
+
+	state_t x = q0::domains::random(dom);
+	state_t y = q0::domains::random(dom);
+
+	auto t = q0::domains::log<float>(dom, x, y);
+	std::cout << "Tangent = {" << t.transpose() << "}" << std::endl;
+
+	state_t q = q0::domains::exp<float>(dom, x, t);
+	auto t2 = q0::domains::log<float>(dom, x, q);
+	std::cout << "Tangent = {" << t2.transpose() << "}" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	{
+		typedef q0::domains::cartesian<float,3> domain_t;
+		domain_t dom;
+		run<domain_t>(dom, "q0::domains::cartesian<float,3>");
+	}
+
+	{
+		typedef q0::domains::vector<q0::domains::so2<float>> domain_t;
+		domain_t dom(3);
+		run<domain_t>(dom, "q0::domains::vector<q0::domains::so2<float>>");
+	}
+
+	{
+		typedef q0::domains::tuple< q0::domains::cartesian<float,3> , q0::domains::so2<float> > domain_t;
+		domain_t dom;
+		run<domain_t>(dom, "q0::domains::tuple< q0::domains::cartesian<float,3> , q0::domains::so2<float> >");
+	}
+
+	{
+		typedef q0::domains::tuple< q0::domains::cartesian<float,3> , q0::domains::vector<q0::domains::so2<float>> > domain_t;
+		domain_t dom;
+		std::get<1>(dom).resize(3);
+		run<domain_t>(dom, "q0::domains::tuple< q0::domains::cartesian<float,3> , q0::domains::vector<q0::domains::so2<float>> >");
+	}
+
+	return 1;
+}
